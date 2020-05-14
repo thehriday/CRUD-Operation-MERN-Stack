1b86f3663fdb9325a225d4b7984fab5adb9d6a5f
MS Hridoy <44698954+mshridoy@users.noreply.github.com>
2020-05-14 13:32:52 +0600

    Update app.js

--- app.js
+++ app.js
@@ -13,12 +13,10 @@ app.use(express.json());
 
 app.use("/api", postRoute);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+});
 
 // handle global error
 app.use((err, req, res, next) => {
